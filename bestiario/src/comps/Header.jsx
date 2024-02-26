7014d18a657e1f8053879e0c0b607dc66edf64d2
Marco Lopez <a1123250023@utch.edu.mx>
2024-02-26 17:38:51 -0600

    Se creo el proyecto Bestiario y le iniciaron los componentes

---
+++ bestiario/src/comps/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+function Header(){
+    return (
+    <div className="header">
+    <h1>Bestiario de Monster Hunter: World</h1>
+    <p>Indispensble para tu caza</p>
+    </div>
+    );
+}
+export default Header;
